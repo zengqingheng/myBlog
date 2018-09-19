356b3be7a1639d4851cbc197524a2c2299d7425e
zengqingheng <34708916+zengqingheng@users.noreply.github.com>
2018-09-19 20:32:43 +0800

    views和模板之前的数据传递
    what：将tag_list传递到模板中，
    why: 做练习
    how：在传入模板的参数中增加tag_list,
    并在模板中修改代码

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from apps.blog.models import Article
-from django.core.paginator import Paginator,PageNotAnInteger, EmptyPage
+from apps.blog.models import Article,Category,Tag
+from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
 from django.http import Http404
 from django.conf import settings
 
@@ -19,7 +19,7 @@ def home(request):
         post_list = paginator.page(1)
     except EmptyPage:
         post_list = paginator.page(paginator.num_pages)
-    return render(request,'home.html',{'post_list':post_list})
+    return render(request,'home.html',{'post_list':post_list,'category_list':categories,'tag_list':tags})
 
 def detail(request,id):
     try:
@@ -28,4 +28,37 @@ def detail(request,id):
         tags = post.tags.all()
     except Article.DoesNotExist:
         raise Http404
-    return render(request,'post.html',{'post':post,'tags':tags})
+    return render(request,'post.html',{'post':post,'tags':tags,'category_list':categories})
+
+categories = Category.objects.all() # 获取全部分类对象
+tags = Tag.objects.all() # 获取全部标签对象
+
+def search_category(request,id): # 分类搜索
+    posts = Article.objects.filter(category_id=str(id))
+    category = categories.get(id=str(id))
+    print('category:',category,type(category))
+    paginator = Paginator(posts,settings.PAGE_NUM)
+    try:
+        page = request.GET.get('page')
+        post_list = paginator.page(page)
+    except PageNotAnInteger:
+        post_list = paginator.page(1)
+    except EmptyPage:
+        post_list = paginator.page(paginator.num_pages)
+    return  render(request,'category.html',{'post_list':post_list,'category_list':categories,'category':category})
+
+def search_tag(request,tag):
+    #posts = Article.objects.filter(tags__name__contains=tag)# contains是将相似的数据提取出来，比如设置标签C,会把所有带C字母的标签全部查询出来
+    posts = Article.objects.filter(tags__name__contains=tag)
+    print('posts:',posts)
+    tag = tags.get(name=tag)
+    print('tag2:', tag, type(tag))
+    paginator = Paginator(posts,settings.PAGE_NUM)
+    try:
+        page = request.GET.get('page')
+        post_list = paginator.page(page)
+    except PageNotAnInteger:
+        post_list = paginator.page(1)
+    except EmptyPage:
+        post_list = paginator.page(paginator.num_pages)
+    return render(request,'tag.html',{'post_list':post_list,'category_list':categories,'tag':tag})
